68c684aa91253c0e9fd1dd34b5e324bdad5fae30
Dan Dart <git@dandart.co.uk>
2020-03-23 23:41:04 +0000

    Link to ham radio page externally

--- src/Html/DanDart/Index.hs
+++ src/Html/DanDart/Index.hs
@@ -131,37 +131,7 @@ pageFavourites = li ! class_ "nav-item" $ do
                     extLink "http://riscos.com/riscos/310/index.php" "RISC OS"
 
 pageHamRadio :: Html
-pageHamRadio = li ! class_ "nav-item" $ do
-    input ! type_ "radio" ! A.style "display:none" ! name "selected" ! A.id "Ham Radio" ! value "Ham Radio"
-    H.label ! class_ "mb-0" ! for "Ham Radio" $ a ! class_ "nav-link btn btn-sm" $ "Ham Radio"
-    H.div ! class_ "page" ! A.id "ham" $ do
-        H.div ! class_ "row" $ H.div ! class_ "col my-md-3" $ small "» Ham Radio"
-        H.div ! class_ "row" $ H.div ! class_ "col-md-8 offset-md-2 py-3 bg-light" $ do
-            p "I am a UK full-licenced radio amateur, and have been issued the callsign M0ORI."
-            p $ do
-                "My nearest radio club is "
-                extLink "https://www.midsarc.org.uk/" "Mid-Somerset Amateur Radio Club"
-                "."
-            p "I own the following types of radio:"
-            ul $ do
-                li $ do
-                    extLink "https://www.yaesu.com/indexVS.cfm?cmd=DisplayProducts&ProdCatID=102&encProdID=06014CD0AFA0702B25B12AB4DC9C0D27" "Yaesu FT-817"
-                    " (5W, all-mode HF, VHF, UHF transceiver)"
-                li $ do
-                    extLink "https://baofengtech.com/uv82" "Baofeng UV-82"
-                    " (5W, FM, VHF and UHF transceiver)"
-                li $ do
-                    extLink "http://www.uv3r.com/" "Baofeng UV-3R"
-                    " (2W, FM, VHF and UHF transceiver)"
-                li $ do
-                    extLink "https://www.eham.net/reviews/detail/7627" "Tecsun PL-600"
-                    " (HF receiver)"
-            p "You may sometimes find me on:"
-            ul $ do
-                li "FM in mid-Somerset, UK (IO81)."
-                li "PSK on usually 20m"
-                li "JT modes on usually 20m"
-            p $ extLink "https://www.qrzcq.com/call/M0ORI" "My QRZCQ page"
+pageHamRadio = li ! class_ "nav-item" $ a ! class_ "nav-link btn btn-sm" ! href "https://m0ori.com" ! target "_blank" $ "Ham Radio"
 
 pageHealth :: Html
 pageHealth = li ! class_ "nav-item" $ do
