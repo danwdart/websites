24aaefdc413353785798cc555a412978bd975f65
Dan Dart <git@dandart.co.uk>
2020-08-16 20:59:29 +0100

    Refactor some blog stuff

--- src/Site/Blog.hs
+++ src/Site/Blog.hs
@@ -134,6 +134,52 @@ makeBlogPost filename = do
     comments <- getComments postId
     return $ BlogPost metadata html comments
 
+commentForm :: FilePath -> Html
+commentForm postId = H.form
+    ! A.class_ "form"
+    ! enctype "application/x-www-form-urlencoded"
+    ! action "https://kkeacv0mpj.execute-api.eu-west-2.amazonaws.com/dev/comment"
+    ! method "post"
+    ! target "_result" $ do
+        H.input ! A.type_ "hidden" ! name "postId" ! value (fromString postId)
+        mapM_ (\(type_, name_, label_, placeholder_) -> H.div ! A.class_ "form-group" $ do
+            H.label ! for name_ $ label_
+            H.input ! A.type_ type_ ! A.class_ "form-control" ! name name_ ! placeholder placeholder_
+            ) [
+                ("text", "name", "Name", "John Smith"),
+                ("email", "email", "Email", "john@smith.com"),
+                ("text", "website", "Website", "https://mydomain.com")
+                ]
+        H.div ! A.class_ "form-group" $ do
+            H.label ! for "name" $ "Comment"
+            H.textarea ! A.class_ "form-control" ! name "comment" ! placeholder "I think..." $ mempty
+        H.div ! A.class_ "form-group" $ do
+            button ! A.type_ "submit" ! A.class_ "btn btn-primary" $ "Submit"
+            H.iframe ! name "_result" ! height "30" ! width "200" ! A.style "border: 0; vertical-align: middle; margin-left: 10px;" $ mempty
+
+renderComment :: ParseCommentResult -> Html
+renderComment ParseCommentResult {
+    commentDate,
+    commentMetadata = BlogCommentMetadata {
+        author,
+        authorEmail,
+        authorUrl
+    },
+    commentHtml
+    } = do
+        small $ do
+            a ! name (fromString (iso8601Show commentDate)) $ mempty
+            a ! href ("mailto:" <> (fromString . T.unpack $ authorEmail)) $ string (T.unpack author)
+            " "
+            when (isJust authorUrl) $
+                a ! href (fromString $ T.unpack $ fromJust authorUrl) $ " (URL)"
+            " said on "
+            a ! href ("#" <> fromString (iso8601Show commentDate)) $ fromString $ iso8601Show commentDate    
+            ":"
+        p commentHtml
+        br
+            
+
 renderPost :: BlogPost -> Html
 renderPost (BlogPost metadata html comments) = do
     let postId = dropExtension $ takeFileName $ Data.List.head $ aliases metadata
@@ -156,51 +202,11 @@ renderPost (BlogPost metadata html comments) = do
     if Data.List.null comments then
         p "No comments at the moment. Be the first to comment!"
     else
-        mapM_ (\ParseCommentResult {
-            commentDate,
-            commentMetadata = BlogCommentMetadata {
-                author,
-                authorEmail,
-                authorUrl
-            },
-            commentHtml
-            } -> do
-                small $ do
-                    a ! name (fromString (iso8601Show commentDate)) $ mempty
-                    a ! href ("mailto:" <> (fromString . T.unpack $ authorEmail)) $ string (T.unpack author)
-                    " "
-                    when (isJust authorUrl) $
-                        a ! href (fromString $ T.unpack $ fromJust authorUrl) $ " (URL)"
-                    " said on "
-                    a ! href ("#" <> fromString (iso8601Show commentDate)) $ fromString $ iso8601Show commentDate    
-                    ":"
-                p commentHtml
-                br
-            ) comments
+        mapM_ renderComment comments
     br
     details $ do
         H.summary $ h4 ! A.class_ "d-inline-block" $ "Post a comment"
-        H.form
-            ! A.class_ "form"
-            ! enctype "application/x-www-form-urlencoded"
-            ! action "https://kkeacv0mpj.execute-api.eu-west-2.amazonaws.com/dev/comment"
-            ! method "post"
-            ! target "_result" $ do
-                H.input ! A.type_ "hidden" ! name "postId" ! value (fromString postId)
-                mapM_ (\(type_, name_, label_, placeholder_) -> H.div ! A.class_ "form-group" $ do
-                    H.label ! for name_ $ label_
-                    H.input ! A.type_ type_ ! A.class_ "form-control" ! name name_ ! placeholder placeholder_
-                    ) [
-                        ("text", "name", "Name", "John Smith"),
-                        ("email", "email", "Email", "john@smith.com"),
-                        ("text", "website", "Website", "https://mydomain.com")
-                        ]
-                H.div ! A.class_ "form-group" $ do
-                    H.label ! for "name" $ "Comment"
-                    H.textarea ! A.class_ "form-control" ! name "comment" ! placeholder "I think..." $ mempty
-                H.div ! A.class_ "form-group" $ do
-                    button ! A.type_ "submit" ! A.class_ "btn btn-primary" $ "Submit"
-                    H.iframe ! name "_result" ! height "30" ! width "200" ! A.style "border: 0; vertical-align: middle; margin-left: 10px;" $ mempty
+        commentForm postId
     hr
     
 
