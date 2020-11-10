41e2dcdcd1c85619470934d28e97ab0db8e072d0
Dan Dart <git@dandart.co.uk>
2020-11-10 09:27:24 +0000

    Use Pandoc so we can set markdown external links and downloads, then do it.

--- src/Blog/Post.hs
+++ src/Blog/Post.hs
@@ -16,11 +16,17 @@ import qualified Data.Text.IO                as TIO
 import           System.FilePath
 import           Text.Blaze.Html5            as H hiding (main)
 import           Text.Blaze.Html5.Attributes as A
+import Text.Pandoc.Class
+-- import Text.Pandoc.Readers.HTML
+import Text.Pandoc.Readers.Markdown
+import Text.Pandoc.Writers.HTML
+import Text.Blaze.Internal
+import Data.Either
 -- import Text.Blaze.Renderer.Utf8
 
 parseFile ∷ Text → ParseResult
 parseFile contents' = case parseYamlFrontmatter (encodeUtf8 contents') of
-    Done i' r -> ParseResult r . toMarkup $ markdown def (decodeUtf8 i')
+    Done i' r -> ParseResult r (fromRight "" $ runPure (writeHtml5 def =<< readMarkdown def (decodeUtf8 i')))
     Fail _ xs y -> error $ "Failure of " <> (show xs <> y)
     _ -> error $ "What is " <> T.unpack contents'
 
@@ -32,6 +38,50 @@ makeBlogPost filename = do
     let postId' = dropExtension $ takeFileName filename
     comments' <- getComments postId'
     return $ BlogPost (T.pack postId') metadata' html' comments'
+
+tshowChoiceString :: ChoiceString -> Text
+tshowChoiceString (Text s) = s
+tshowChoiceString _ = ""
+
+isLink :: StaticString -> Bool
+isLink ss1 = "a" == getText ss1
+
+isExternalLink :: ChoiceString -> Bool
+isExternalLink acs = "http" `T.isPrefixOf` tshowChoiceString acs
+
+isFeed :: ChoiceString -> Bool
+isFeed acs = ".xml" `T.isSuffixOf` tshowChoiceString acs
+
+setExternalLink :: MarkupM a -> MarkupM a
+setExternalLink (AddAttribute ass1 ass2 acs res) =
+    AddAttribute "target" "target=\"" "_blank" .
+        AddAttribute "rel" "rel=\"" "noreferrer" $
+            AddAttribute ass1 ass2 acs res
+setExternalLink as = as
+
+setDownload :: MarkupM a -> MarkupM a
+setDownload (AddAttribute ass1 ass2 acs res) =
+    AddAttribute "download" "download=\"" "" $
+        AddAttribute ass1 ass2 acs res
+setDownload as = as
+
+fixExternalLinks :: MarkupM a -> MarkupM a
+fixExternalLinks at@(AddAttribute _ _ acs (Parent ss1 _ _ _)) =
+    if isLink ss1 then
+        if isExternalLink acs
+        then
+            setExternalLink at
+        else
+            if isFeed acs
+            then
+                setDownload at
+            else
+                at
+    else
+        at
+fixExternalLinks (Parent ss1 ss2 ss3 res) = Parent ss1 ss2 ss3 (fixExternalLinks res)
+fixExternalLinks (Append m1 m2) = Append (fixExternalLinks m1) (fixExternalLinks m2)
+fixExternalLinks as = as
 
 renderPost ∷ BlogPost → Html
 renderPost (BlogPost postId' metadata' html' comments') = do
@@ -50,7 +100,7 @@ renderPost (BlogPost postId' metadata' html' comments') = do
             ) . T.unpack . getTag) (tags metadata')
     br
     br
-    html'
+    fixExternalLinks html'
     br
     h3 "Comments"
     if Data.List.null comments' then
