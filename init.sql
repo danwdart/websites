6eed2135dc35e43c86e270e4a01f8ce7c319eb78
Dan Dart <git@dandart.co.uk>
2020-11-25 20:51:50 +0000

    some visits fixes

--- init.sql
+++ init.sql
@@ -1,7 +1,9 @@
-CREATE TABLE IF NOT EXISTS `visits` (
-    `id` INT PRIMARY KEY AUTOINCREMENT,
-    `url` TEXT,
-    `ua` TEXT,
-    `ip` TEXT,
-    `time` TIMESTAMP
+CREATE DATABASE `visits`;
+GRANT ALL ON `visits`.`*` TO `visits`;
+CREATE TABLE IF NOT EXISTS `visits`.`visits` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `url` TEXT NOT NULL,
+    `ua` TEXT NOT NULL,
+    `ip` TEXT NOT NULL,
+    `time` TIMESTAMP NOT NULL
 );
