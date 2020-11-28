ed2b646cf9262f0b7ab104d67574b604bfe95896
Dan Dart <git@dandart.co.uk>
2020-11-28 07:21:20 +0000

    escape user input (todo use other lib)

--- src/visit.hs
+++ src/visit.hs
@@ -18,12 +18,13 @@ import           Data.ByteString.Char8       (ByteString)
 import qualified Data.ByteString.Char8       as B
 import           Data.Text                   as T (Text)
 import           Data.Time                   (defaultTimeLocale, formatTime, UTCTime, getCurrentTime)
-import           Database.MySQL.Base         (ConnectInfo (connectDatabase, connectHost, connectPassword, connectUser),
+import           Database.MySQL.Base         (escape, ConnectInfo (connectDatabase, connectHost, connectPassword, connectUser),
                                               connect, defaultConnectInfo,
                                               query)
 import           Network.AWS.Lens            ((&), (.~), (?~), (^.))
 import           System.Environment          (getEnv)
 import           Text.Printf                 (printf)
+import Data.Maybe (fromMaybe)
 
 main ∷ IO ()
 main = apiGatewayMain handler
@@ -39,15 +40,17 @@ handler request = do
     username <- getEnv "DB_USERNAME"
     password <- getEnv "DB_PASSWORD"
     time <- formatToMySQL <$> getCurrentTime
-    let ua = maybe "" B.unpack $ request ^. agprqHeaders & lookup "User-Agent"
-    let ip = maybe "" show $ request ^. agprqRequestContext . prcIdentity . riSourceIp
-    let url = maybe "" (maybe "" B.unpack) (request ^. agprqQueryStringParameters & lookup "url")
     conn <- connect defaultConnectInfo {
         connectHost = "websites-dev-visitsdb-wt73yj8godix.cluster-c3bfry1faakf.eu-west-2.rds.amazonaws.com",
         connectUser = username,
         connectPassword = password,
         connectDatabase = "visits"
     }
+    let ip = maybe "" show $ request ^. agprqRequestContext . prcIdentity . riSourceIp
+    sua <- escape conn . fromMaybe "" $ request ^. agprqHeaders & lookup "User-Agent"
+    surl <- escape conn . maybe "" (fromMaybe "") $ request ^. agprqQueryStringParameters & lookup "url"
+    let ua = B.unpack sua
+    let url = B.unpack surl
     query conn . B.pack $ printf "INSERT INTO `visits`.`visits` (url, ua, ip, time) values (\"%s\", \"%s\", \"%s\", \"%s\")" url ua ip time
     pure $ responseOK
       & agprsHeaders .~ [("Content-Type", "text/plain")]
