786e0c7f97c7733c43370eee0d28482a43d4b07c
Dan Dart <git@dandart.co.uk>
2020-11-29 12:49:39 +0000

    lint partially

--- src/Util/String.hs
+++ src/Util/String.hs
@@ -1,3 +1,3 @@
 {-# LANGUAGE UnicodeSyntax #-}
-(<<>>) ∷ (IsString a, IsString b, IsString c) → a -> b -> c
+(<<>>) ∷ (IsString a, IsString b, IsString c) → a → b -> c
 a <<>> b = fromString a <> fromString b
