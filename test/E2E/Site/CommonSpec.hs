bc49bc1edfb9d9bae09cf7f6fce497039acfdd67
Dan Dart <git@dandart.co.uk>
2021-03-23 11:08:45 +0000

    make tests clearer

--- test/E2E/Site/CommonSpec.hs
+++ test/E2E/Site/CommonSpec.hs
@@ -9,15 +9,17 @@ import           Control.Concurrent.Async         (mapConcurrently)
 import           Control.Exception                (SomeException (SomeException),
                                                    try)
 import           Control.Monad                    (when)
+import           Control.Monad.IO.Class
 import           Data.Aeson                       (Object,
                                                    Value (Array, String),
                                                    object)
 import           Data.Functor.Compose             (Compose (Compose, getCompose))
 import           Data.List                        (nub)
 import           Data.Maybe                       (catMaybes)
-import           Data.Text                        (unpack)
+import           Data.Text                        (Text, unpack)
 import           Data.Vector                      (fromList)
-import           Network.HTTP.Client              (Response (responseStatus),
+import           Network.HTTP.Client              (Manager,
+                                                   Response (responseStatus),
                                                    httpNoBody, newManager,
                                                    parseRequest)
 import           Network.HTTP.Client.TLS          (tlsManagerSettings)
@@ -28,34 +30,37 @@ import qualified Site.JolHarg                     as J
 import qualified Site.M0ORI                       as M
 import qualified Site.MadHacker                   as MH
 import           System.Environment               (setEnv)
+import           System.Process
 import           System.Random                    (Random (randomRIO))
-import           Test.Hspec                       (HasCallStack, Spec, xdescribe, describe,
-                                                   it, parallel, runIO,
-                                                   shouldBe, shouldSatisfy)
+import           Test.Hspec                       (HasCallStack, Spec, describe,
+                                                   hspec, it, runIO, shouldBe,
+                                                   shouldSatisfy)
 import           Test.Hspec.Expectations          (shouldNotBe,
                                                    shouldNotContain)
 import           Test.WebDriver                   (Browser (chromeOptions),
                                                    Capabilities (browser),
+                                                   Element,
                                                    Selector (ByCSS, ByClass),
+                                                   WD,
                                                    WDConfig (wdCapabilities),
                                                    additionalCaps, attr, chrome,
-                                                   click, closeSession,
-                                                   currentWindow, defaultCaps,
-                                                   defaultConfig, elemSize,
+                                                   click, currentWindow,
+                                                   defaultCaps, defaultConfig,
+                                                   elemSize, finallyClose,
                                                    findElem, findElems, getText,
                                                    openPage)
 import           Test.WebDriver.Class             (WebDriver, methodPost)
 import           Test.WebDriver.Commands.Internal (doWinCommand)
+import           Test.WebDriver.Config            (WebDriverConfig)
 import           Test.WebDriver.JSON              (pair)
-import           Test.WebDriver.Monad             (runSession, runWD)
-import           Test.WebDriver.Session           (WDSessionState (getSession))
+import           Test.WebDriver.Monad             (runSession)
 
 firefoxConfig ∷ WDConfig
 firefoxConfig = defaultConfig {
     wdCapabilities = defaultCaps {
         additionalCaps = [
             ("moz:firefoxOptions", object [
-                ("args", Array (fromList [String "--headless"]))
+                ("args", Array (fromList [String ""]))
             ])
         ]
     }
@@ -65,7 +70,7 @@ chromeConfig ∷ WDConfig
 chromeConfig = defaultConfig {
     wdCapabilities = defaultCaps {
         browser = chrome {
-            chromeOptions = ["--headless"]
+            chromeOptions = []
         }
     }
 }
@@ -94,13 +99,13 @@ resolutions = [
     (1920, 1080)
     ]
 
-configs ∷ [(String, WDConfig)]
+configs ∷ [(Text, WDConfig)]
 configs = [
-    ("Firefox", firefoxConfig),
-    ("Chrome", chromeConfig)
+    ("Firefox", firefoxConfig)
+    --("Chrome", chromeConfig)
     ]
 
-sites ∷ [(String, IO ())]
+sites ∷ [(Text, IO ())]
 sites = [
     ("blog", B.serve True),
     ("dandart", D.serve True),
@@ -113,96 +118,98 @@ sites = [
 ioDef ∷ a → IO a → IO a
 ioDef d io = either (\(SomeException _) -> d) id <$> try io
 
+testForLink ∷ Element → WD ()
+testForLink linkToClick = do
+    linkName <- getText linkToClick
+    cardSizes <- do
+        click linkToClick
+        cards <- findElems $ ByClass "card"
+        mapM elemSize cards
+
+    liftIO . hspec . describe (unpack linkName) . it "visible cards are only one size" $ (
+        (length . nub . filter (/= (0, 0)) $ cardSizes )`shouldSatisfy` (< 2))
+
+testForResolution ∷ Text → (Word, Word) → WD ()
+testForResolution siteName winSize = do
+    navHeight <- do
+        _ <- setWindowSize winSize
+        navbar <- findElem $ ByClass "navbar-nav"
+        (_, navHeight) <- elemSize navbar
+        pure navHeight
+
+    liftIO . hspec $ describe (showTuple winSize) $
+        it "nav height is equal to 39" $
+            navHeight `shouldBe` 39
+
+    -- only care about cards in JolHarg, but it's an option later.
+    when ("jolharg" == siteName) $ do
+        links <- findElems $ ByCSS ".navbar-nav label a"
+        mapM_ testForLink links
+
+testSecureLink ∷ String → Spec
+testSecureLink src = describe src .
+    it "is secure" $
+        src `shouldNotContain` "http:"
+
+getStatuses ∷ Manager → String → IO (String, Int)
+getStatuses manager url = ioDef (url, 0) $ do
+    request <- parseRequest url
+    response <- httpNoBody request manager
+    pure (url, statusCode . responseStatus $ response)
+
+testNotBroken ∷ (String, Int) → Spec
+testNotBroken (url, status) = describe url $ do
+    it "should not be failing" $
+        status `shouldNotBe` 0
+    it "should not 404" $
+        status `shouldNotBe` 404
+
+
+testForConfig ∷ WebDriverConfig conf ⇒ Int → Text → (Text, conf) → Spec
+testForConfig myPort siteName (configName, config) =
+    describe (unpack configName) $ do
+        runIO . runSession config . finallyClose $ do
+            openPage $ "http://" <> unpack siteName <> ".localhost:" <> show myPort
+
+            -- only the first option
+            when ("Firefox" == configName) $ do
+                urls <- do
+                    as <- findElems (ByCSS "a[href^=http]")
+                    hrefs <- mapM (`attr` "href") as
+                    pure (catMaybes $ getCompose (unpack <$> Compose hrefs))
+
+                images <- do
+                    as <- findElems (ByCSS "img[src^=http]")
+                    srcs <- mapM (`attr` "src") as
+                    pure (catMaybes $ getCompose (unpack <$> Compose srcs))
+
+                manager <- liftIO $ newManager tlsManagerSettings
+
+                urlStatuses <- liftIO $ mapConcurrently (getStatuses manager) urls
+
+                imageStatuses <- liftIO $ mapConcurrently (getStatuses manager) images
+
+                liftIO . hspec $ do
+                    describe "has no insecure images" $ mapM_ testSecureLink images
+                    describe "has no broken links" $ mapM_ testNotBroken urlStatuses
+                    describe "has no missing images" $ mapM_ testNotBroken imageStatuses
+
+            mapM_ (testForResolution siteName) resolutions
+
+testForSite ∷ (Text, IO ()) → Spec
+testForSite (siteName, serve) = describe (unpack siteName) $ do
+    myPort <- runIO (randomRIO (49152, 65535) :: IO Int)
+    thread <- runIO $ do
+        setEnv "PORT" (show myPort)
+        thread <- forkIO serve
+        threadDelay 3000000 -- Let it start
+        pure thread
+
+    mapM_ (testForConfig myPort siteName) configs
+    runIO $ killThread thread
+
+
 spec ∷ Spec
-spec = parallel $ mapM_ (\(siteName, serve) ->
-    parallel . xdescribe siteName $ do
-        myPort <- runIO (randomRIO (49152, 65535) :: IO Int)
-        thread <- runIO $ do
-            setEnv "PORT" (show myPort)
-            thread <- forkIO serve
-            threadDelay 3000000 -- Let it start
-            pure thread
-
-        mapM_ (\(configName, config) -> do
-            parallel . xdescribe configName $ do
-                session <- runIO . runSession config $ do
-                    openPage $ "http://" <> siteName <> ".localhost:" <> show myPort
-                    getSession
-
-                -- only the first option
-                when ("Firefox" == configName) $ do
-                    urls <- runIO . runWD session $ do
-                        as <- findElems (ByCSS "a[href^=http]")
-                        hrefs <- mapM (`attr` "href") as
-                        pure (catMaybes $ getCompose (unpack <$> Compose hrefs))
-
-                    images <- runIO . runWD session $ do
-                        as <- findElems (ByCSS "img[src^=http]")
-                        srcs <- mapM (`attr` "src") as
-                        pure (catMaybes $ getCompose (unpack <$> Compose srcs))
-
-                    parallel . describe "has no insecure images" $ mapM_ (\src ->
-                        describe src .
-                            it "is not insecure" $
-                                src `shouldNotContain` "http:"
-                        ) images
-
-                    manager <- runIO $ newManager tlsManagerSettings
-
-                    urlStatuses <- runIO $ mapConcurrently (\url -> ioDef (url, 0) $ do
-                        request <- parseRequest url
-                        response <- httpNoBody request manager
-                        pure (url, statusCode . responseStatus $ response)
-                        ) urls
-
-                    imageStatuses <- runIO $ mapConcurrently (\src -> ioDef (src, 0) $ do
-                        request <- parseRequest src
-                        response <- httpNoBody request manager
-                        pure (src, statusCode . responseStatus $ response)
-                        ) images
-
-                    parallel . describe "has no broken links" $ mapM_ (\(url, status) ->
-                        parallel . describe url $ do
-                            it "should not be failing" $
-                                status `shouldNotBe` 0
-                            it "should not 404" $
-                                status `shouldNotBe` 404
-                        ) urlStatuses
-
-                    parallel . describe "has no missing images" $ mapM_ (\(src, status) ->
-                        parallel . describe src $ do
-                            it "should not be failing" $
-                                status `shouldNotBe` 0
-                            it "should not 404" $
-                                status `shouldNotBe` 404
-                        ) imageStatuses
-
-                mapM_ (\winSize -> do
-                    describe (showTuple winSize) $ do
-                        navHeight <- runIO . runWD session $ do
-                            _ <- setWindowSize winSize
-                            navbar <- findElem $ ByClass "navbar-nav"
-                            (_, navHeight) <- elemSize navbar
-                            pure navHeight
-
-                        it "nav height is equal to 39" $
-                            navHeight `shouldBe` 39
-
-                        -- only care about cards in JolHarg, but it's an option later.
-                        when ("jolharg" == siteName) $ do
-                            links <- runIO . runWD session . findElems $ ByCSS ".navbar-nav label a"
-                            mapM_ (\linkToClick -> do
-                                linkName <- runIO . runWD session $ getText linkToClick
-                                cardSizes <- runIO . runWD session $ do
-                                    click linkToClick
-                                    cards <- findElems $ ByClass "card"
-                                    mapM elemSize cards
-
-                                parallel . describe (unpack linkName) . it "visible cards are only one size" $ (
-                                    (length . nub . filter (/= (0, 0)) $ cardSizes )`shouldSatisfy` (< 2))
-                                ) links
-                        ) resolutions
-                runIO . runWD session $ closeSession
-                ) configs
-        runIO $ killThread thread
-    ) sites
+spec = runIO $ withCreateProcess (shell "selenium-server") $ \_ _ _ _ -> do
+    threadDelay 5000000
+    hspec $ mapM_ testForSite sites
