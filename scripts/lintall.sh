b57052bf97bb0f0e821629bbf4e3aa9a8b012a7d
Dan Dart <git@dandart.co.uk>
2021-07-20 03:55:35 +0100

    silence lintall

--- scripts/lintall.sh
+++ scripts/lintall.sh
@@ -1,3 +1,4 @@
 FILES=$(find . -name "*.hs" | grep -v .stack-work | grep -v dist-newstyle)
+echo "will cite" | parallel --citation 2> /dev/null
 parallel --halt never hlint --refactor --refactor-options=-i ::: $FILES
 parallel --halt never stylish-haskell -i ::: $FILES
