1a67ca08777fa4c4888d51854b08623617412230
Dan Dart <git@dandart.co.uk>
2021-09-09 01:56:15 +0100

    Update nixpkgs and compiler - fix default.nix

--- default.nix
+++ default.nix
@@ -1,21 +1,16 @@
 { 
-  nixpkgs ? import <nixpkgs> {},
-  unstable ? import <unstable> {},
-  compiler ? "ghc8104", # basement doesn't yet support 901
-  ghcjs ? "ghcjs86"
+  nixpkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/master.tar.gz") {},
+  compiler ? "ghc8107" # basement doesn't yet support 901
 } :
 let
   gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
-  myHaskellPackages = unstable.pkgs.haskell.packages.${compiler}.override {
+  myHaskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: rec {
       websites = self.callCabal2nix "websites" (gitignore ./.) {};
       fsutils = self.callCabal2nix "fsutils" (builtins.fetchGit {
         url = "https://github.com/danwdart/fsutils.git";
         rev = "bd85f977a7499a936181a37f4c602bd8b4480d68";
       }) {};
-      mmark = (self.callHackage "mmark" "0.0.7.2" {}).overrideDerivation(self: {
-        doCheck = false; # It fails but I don't care as it's nothing I use, just a dependency of a part of a package I don't use.
-      });
     };
   };
   shell = myHaskellPackages.shellFor {
@@ -38,9 +33,9 @@ let
       parallel
       wget
     ];
-    withHoogle = true;
+    withHoogle = false;
   };
-  exe = unstable.haskell.lib.justStaticExecutables (myHaskellPackages.websites);
+  exe = nixpkgs.haskell.lib.justStaticExecutables (myHaskellPackages.websites);
 in
 {
   inherit shell;
