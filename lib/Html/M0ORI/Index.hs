0116ce9b3e0933ecabf2d9a42c6d5106fe6c5bfe
Dan Dart <git@dandart.co.uk>
2021-10-24 20:39:04 +0100

    update to radio page

--- lib/Html/M0ORI/Index.hs
+++ lib/Html/M0ORI/Index.hs
@@ -23,33 +23,19 @@ pageHamRadio = makePage "ham" "Ham Radio" defaultLayout defaultPage $ do
         "My nearest radio club is "
         extLink "https://www.midsarc.org.uk/" "Mid-Somerset Amateur Radio Club"
         "."
-    p "I own the following types of radio:"
-    ul $ do
-        li $ do
-            extLink "https://www.yaesu.com/indexVS.cfm?cmd=DisplayProducts&ProdCatID=102&encProdID=06014CD0AFA0702B25B12AB4DC9C0D27" "Yaesu FT-817"
-            " (5W, all-mode HF, VHF, UHF transceiver)"
-        li $ do
-            extLink "https://www.amazon.co.uk/BaoFeng-UV-5R-Radio-Walkie-Talkie/dp/B072HXQ5GG" "Baofeng UV-5R 8W"
-            " (8W, FM, VHF and UHF transceiver)"
-        li $ do
-            extLink "https://baofengtech.com/uv82" "Baofeng UV-82"
-            " (5W, FM, VHF and UHF transceiver)"
-        li $ do
-            extLink "http://www.uv3r.com/" "Baofeng UV-3R"
-            " (2W, FM, VHF and UHF transceiver)"
-        li $ do
-            extLink "https://www.eham.net/reviews/detail/7627" "Tecsun PL-600"
-            " (HF receiver)"
+    p $ do
+        "I work on a "
+        extLink "https://www.yaesu.com/indexVS.cfm?cmd=DisplayProducts&ProdCatID=102&encProdID=06014CD0AFA0702B25B12AB4DC9C0D27" "Yaesu FT-817"
+        " (5W, all-mode HF, VHF, UHF transceiver)"
     p "You may sometimes find me on:"
     ul $ do
-        li "FM in mid-Somerset, UK (IO81)."
-        li "PSK on usually 20m"
-        li "JT modes on usually 20m"
+        li "FM on VHF/UHF in mid-Somerset, UK (IO81)."
+        li "FT modes and occasionally PSK on HF"
     p $ extLink "https://www.qrzcq.com/call/M0ORI" "My QRZCQ page"
     br
     (H.div ! A.id "rigref-solar-widget")
-        . (a ! href "https://rigreference.com/solar" ! target "_blank")
-        $ (img ! src "https://rigreference.com/solar/img/tall")
+        . (a ! href "https://www.hamqsl.com/solar.html" ! target "_blank")
+        $ (img ! src "https://www.hamqsl.com/solar101vhf.php")
 
 pageContact ∷ WebsiteM Html
 pageContact = do
