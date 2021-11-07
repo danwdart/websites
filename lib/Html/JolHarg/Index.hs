636824b5e4399f8220f37156aa338bcf92e8a7ac
Dan Dart <git@dandart.co.uk>
2021-11-07 20:50:24 +0000

    Fix cards JH

--- lib/Html/JolHarg/Index.hs
+++ lib/Html/JolHarg/Index.hs
@@ -22,19 +22,26 @@ import           Text.Blaze.Html5.Attributes as A
 pagePortfolio ∷ WebsiteM Html
 pagePortfolio = makePage "portfolio" "Portfolio" customLayout defaultPage $ do
     row . (H.div ! class_ "col-md-12 text-center") $
-        p "Some of the websites and projects JolHarg Ltd has been involved with are:"
+        p "Some of the websites, projects and companies JolHarg Ltd has been involved with are:"
     row $ do
-        (H.div ! class_ "card col-md-4 text-center") . (H.div ! class_ "card-body") $ (do
+        (H.div ! class_ "card col-md-4 col-12 text-center") . (H.div ! class_ "card-body") $ (do
             img ! class_ "card-img-top" ! src "img/sample.png"
             h4 ! class_ "card-title" $ "You"
             p ! class_ "card-text" $ "Make an enquiry for a website or project:"
             extLink "mailto:website@jolharg.com" ! class_ "btn btn-primary" $ "Enquire")
+        card "img/timezap.png" "TimeZap" "Set-and-forget accurate time tracking" "https://timezap.ai"
+        card "img/eppiq.png" "Eppiq Marketing" "Digital marketing agency" "https://eppiq.co.uk"
+        card "img/penta.png" "Penta Consulting" "Web Agency" "https://www.pentaconsulting.com/"
+        card "img/dotfive.png" "DotFive" "Promotional website" "https://dotfive.co.uk"
+        card "img/usaycompare.png" "Usaycompare" "Health insurance comparison" "https://usaycompare.co.uk"
+        card "img/future.png" "Future Publishing" "Technology publication" "https://futureplc.com"
         card "img/techradar.png" "TechRadar" "Technology website" "https://techradar.com/"
         card "img/feeld.png" "Feeld" "Dating app for singles and couples" "https://feeld.co/"
         card "img/polaris.png" "Polaris Elements" "Hospitality software" "https://polaris-elements.co.uk/"
         cardDefunct "Plugin ASO" "Analytics dashboard for Shopify"
         card "img/faultfixers.png" "FaultFixers" "Facilities management" "https://faultfixers.com"
-        card "img/dadi.png" "DADI" "DADI web services suite" "https://dadi.cloud/en/"
+        -- https://dadi.cloud/en/ is currently broken
+        card "img/dadi.png" "DADI" "DADI web services suite" "https://docs.dadi.cloud/"
         card "img/planetradio.png" "Planet Radio" "Collection of UK radio magazine websites" "https://planetradio.co.uk/"
         card "img/kompli.png" "Kompli Global" "Due diligence and search intelligence" "https://kompli-global.com"
         card "img/canddi.png" "CANDDi" "Smart web analytics" "https://canddi.com"
@@ -44,7 +51,8 @@ pagePortfolio = makePage "portfolio" "Portfolio" customLayout defaultPage $ do
         card "img/soampli.png" "SoAmpli" "Social media amplification" "https://soampli.com"
         card "img/viewex.png" "Viewex" "Advertising revenue optimisation" "http://viewex.co.uk"
         card "img/mobilefun.png" "Mobile Fun" "Web shop for phones and accessories" "http://mobilefun.co.uk"
-        card "img/gamingzap.png" "Gamingzap" "Web shop for gaming devices and accessories" "http://gamingzap.com"
+        -- gamingzap now redirects to mobilefun
+        card "img/gamingzap.png" "Gamingzap" "Web shop for gaming devices and accessories" "https://www.mobilefun.co.uk/"
         card "img/gearzap.png" "Gearzap" "Web shop for cases and accessories" "http://gearzap.com"
         card "img/rattray.png" "Rattray Mosaics" "Portfolio and personal website of local mosaic artist" "http://rattraymosaics.co.uk"
         card "img/smdaf.png" "Shepton Mallet Digital Arts Festival" "Local festival site" "http://sheptondigitalarts.co.uk"
