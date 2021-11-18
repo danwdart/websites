6081ceb4211716fb1e804110a91523893670d9b4
Dan Dart <git@dandart.co.uk>
2021-11-18 20:42:00 +0000

    Scan everything but dist-newstyle for krank

--- default.nix
+++ default.nix
@@ -33,7 +33,7 @@ let
     ];
     shellHook = ''
       gen-hie > hie.yaml
-      for i in $(find -type f); do krank $i; done
+      for i in $(find -type f | grep -v dist-newstyle); do krank $i; done
     '';
     buildInputs = with nixpkgs; with haskellPackages; [
       apply-refact
