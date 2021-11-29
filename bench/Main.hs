e825678e666a596879d90ebec739587331f74272
Dan Dart <git@dandart.co.uk>
2021-11-29 19:56:05 +0000

    add build benchmarks

---
+++ bench/Main.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import Build
+-- https://hackage.haskell.org/package/criterion
+-- http://www.serpentine.com/criterion/tutorial.html
+import Criterion.Main
+
+main :: IO ()
+main = defaultMain [
+    bgroup "build" [
+        bench "build" $ nfIO runBuild
+    ]
+    ]
