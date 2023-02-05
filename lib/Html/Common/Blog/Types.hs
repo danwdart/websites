28ef762128d04f0a9540e9d72036b5cd3e73a46e
Dan Dart <git@dandart.co.uk>
2023-02-05 13:40:22 +0000

    huh?

--- lib/Html/Common/Blog/Types.hs
+++ lib/Html/Common/Blog/Types.hs
@@ -86,5 +86,6 @@ instance FromJSON BlogMetadata where
         o .: "aliases" <*>
         o .:? "featuredImage" <*>
         (concat <$> (o .:? "tags")) <*> -- Maybe [a] -> [a]
+         -- Maybe [a] -> [a]
         o .:? "scores"
     parseJSON _ = fail "Bad blog metadata"
