ca174aca87c71ff57580b33344941182a194e902
Dan Dart <git@dandart.co.uk>
2023-09-04 18:12:45 +0100

    just have gh actions to build and push cache artifacts instead of actually building it for now

--- scripts/pushall.sh
+++ scripts/pushall.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 nix-build shell.nix
 nix-store -qR --include-outputs $(nix-instantiate shell.nix --add-root result) | cachix push dandart
+nix-store -qR --include-outputs $(nix-instantiate shell.nix --add-root result) | xargs nix-copy-closure --gzip -s --include-outputs cache.jolharg.com
