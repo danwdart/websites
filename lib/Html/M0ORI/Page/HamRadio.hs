d1ef294e46eb74e6aa74428b3b9b551983701ce4
Dan Dart <git@dandart.co.uk>
2024-07-18 17:18:07 +0100

    update m0ori detail page

--- lib/Html/M0ORI/Page/HamRadio.hs
+++ lib/Html/M0ORI/Page/HamRadio.hs
@@ -13,16 +13,16 @@ pageHamRadio ∷ MonadReader Website m ⇒ m Html
 pageHamRadio = makePage "ham" "Ham Radio" defaultLayout defaultPage $ do
     p "I am a UK full-licenced radio amateur, and have been issued the callsign M0ORI."
     p $ do
-        "My nearest radio club is "
-        extLink "https://www.midsarc.org.uk/" "Mid-Somerset Amateur Radio Club"
-        "."
+        "My nearest radio club is Exmouth Amateur Radio Club, but they don't seem to have a website. As far as I have been made aware, they meet on the third Wednesday of the month in Marpool Hill Scout Headquarters at 19:30, although I personally cannot attend for mobility and caring reasons."
     p $ do
         "I work on a "
+        extLink "https://www.baofengradio.co.uk/uv-5r-black-vhf-uhf/" "Baofeng UV-5R"
+        " (8W FM VHF/UHF transceiver) and a "
         extLink "https://www.yaesu.com/indexVS.cfm?cmd=DisplayProducts&ProdCatID=102&encProdID=06014CD0AFA0702B25B12AB4DC9C0D27" "Yaesu FT-817"
         " (5W, all-mode HF, VHF, UHF transceiver)"
     p "You may sometimes find me on:"
     ul $ do
-        li "FM on VHF/UHF in mid-Somerset, UK (IO81)."
+        li "FM/APRS/AX.25/FreeDV on VHF/UHF in East Devon, UK (IO80)."
         li "FT modes and occasionally PSK on HF"
     p $ extLink "https://www.qrzcq.com/call/M0ORI" "My QRZCQ page"
     br
