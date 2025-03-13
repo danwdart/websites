b760274c142873b7649045fb252f8eac847ee951
Dan Dart <git@dandart.co.uk>
2025-03-13 22:28:42 +0000

    root files

---
+++ shell-musl.nix
@@ -0,0 +1,15 @@
+with import <nixpkgs> {};
+runCommand "websites" {
+    buildInputs = [
+        pkgsStatic.gcc
+        pkgsStatic.libffi
+        pkgsStatic.gmp
+        musl
+        # pkgsStatic.haskell.compiler.ghc9
+        # unstable.pkgsStatic.pkgsBuildHost.haskell.compiler.ghc912
+        # pkgsCross.x86_64-embedded.pkgsBuildHost.haskell.compiler.ghc912
+        #pkgsStatic.pkgsBuildHost.haskell.compiler.ghc912
+        (pkgsMusl.haskell.compiler.ghc912.overrideDerivation (f: { enableRelocatedStaticLibs = true; }))
+        cabal-install
+    ];
+} ""
--- shell.nix
+++ shell.nix
@@ -1,1 +1,14 @@
-(import ./default.nix {}).shell
+with import <nixpkgs> {};
+# needs mkShell in order to use headers/etc. from deps! how do we do that from nix-shell 
+mkShell rec {
+    packages = [
+        zlib.dev
+        haskell.compiler.ghc912
+        haskell.packages.ghc910.cabal-install
+    ];
+    # maybe we can include the copy to store stuff in here? as mkShell is a custom stdenv.mkDerivation
+    # shellHook = ''
+    #     export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
+    #     export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH"
+    # '';
+}
